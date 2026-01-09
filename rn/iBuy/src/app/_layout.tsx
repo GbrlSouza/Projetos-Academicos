86484dcdda17145dda906f9a41865bf33bd902c0
gabriel souza <gabrielsouza_1909@hotmail.com>
2026-01-09 13:11:59 -0300

    Tabs config

--- rn/iBuy/src/app/_layout.tsx
+++ rn/iBuy/src/app/_layout.tsx
@@ -1,5 +1,16 @@
+import { MaterialIcons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function Layout() {
-    return <Tabs screenOptions={{ headerShown: false }} />
+    return <Tabs screenOptions={{ headerShown: false, tabBarLabelPosition: 'beside-icon' }}>
+        <Tabs.Screen name="index" options={{ 
+            tabBarLabel: 'Sign In',
+            tabBarIcon: ({ color, size }) => <MaterialIcons name="home" color={color} size={size} />
+            
+        }} />
+        <Tabs.Screen name="sign-up" options={{ 
+            tabBarLabel: 'Sign Up',
+            tabBarIcon: ({ color, size }) => <MaterialIcons name="person-add" color={color} size={size} />
+        }} />
+    </Tabs>
 }
