88dd2c4401b13390047cb3dc2f6d3774cf6913bd
gabriel souza <gabrielsouza_1909@hotmail.com>
2026-01-09 11:49:21 -0300

    Tabs

--- rn/iBuy/src/app/_layout.tsx
+++ rn/iBuy/src/app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from 'expo-router'
+import { Tabs } from 'expo-router'
 
 export default function Layout() {
-    return <Stack screenOptions={{ headerShown: false }} />
+    return <Tabs screenOptions={{ headerShown: false }} />
 }
