2c404366fe914ddaadbf44326e1d32ef9d39915a
gabriel souza <gabrielsouza_1909@hotmail.com>
2026-01-09 09:39:29 -0300

    base

---
+++ rn/iBuy/src/app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello, World!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+    },
+    
+    text: {
+        fontSize: 24,
+        color: '#333',
+    }
+})
